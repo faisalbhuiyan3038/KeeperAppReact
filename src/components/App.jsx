052b706ef14fa495cac74805e45c7a66f8dc0825
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2023-12-31 15:47:16 +0600

    finished Keeper app part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
+import notes from '../notes.js';
 
 function App(){
     return <div>
     <Header></Header>
-        <Note></Note>
+        {notes.map(x => {
+           return <Note 
+                key={x.key}
+                title={x.title}
+                content={x.content}
+            />
+        })}
         <Footer></Footer>
     </div>
 }
